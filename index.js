7eab6074938f2aea6e5f00363b098d63fb075a15
sukanyagovindan <gsukanya@gmail.com>
2022-07-10 10:10:48 -0700

    submit assignment

--- index.js
+++ index.js
@@ -26,19 +26,3 @@ const titleCased = () => {
   return title;
     
 }
-/*
-import java.util.Arrays;
-public class ArrayOfStrings {
-   public static void main(String args[]) {
-      String stringArray[] = {"Hello ", " how", " are", " you", " welcome", " to", " Tutorialspoint"};
-      StringBuffer sb = new StringBuffer();
-      for(int i = 0; i < stringArray.length; i++) {
-         sb.append(stringArray[i]);
-      }
-      String str = Arrays.toString(stringArray);
-      System.out.println(str);
-   }
-}
-
-
-*/ 
